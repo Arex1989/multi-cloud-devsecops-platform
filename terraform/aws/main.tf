32f54efcb4025c02ce0b94a75e36b973f73c7fcc
Arex1989 <rexmond.anih@gmail.com>
2026-09-12 23:19:11 +0200

    security: disable automatic public IP assignment

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_cidr
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "multicloud-devsecops-${var.environment}-public-subnet"
