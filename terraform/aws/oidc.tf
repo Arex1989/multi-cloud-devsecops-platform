a2b1d80987c1153f639e22fe9a96dd3ff13ab2c6
Arex1989 <rexmond.anih@gmail.com>
2026-09-17 07:55:47 +0200

    security: allow Terraform CI to read security group rules

--- terraform/aws/oidc.tf
+++ terraform/aws/oidc.tf
@@ -78,6 +78,7 @@ resource "aws_iam_role_policy" "github_actions_read_infrastructure" {
           "ec2:DescribeRouteTables",
           "ec2:DescribeInternetGateways",
           "ec2:DescribeSecurityGroups",
+          "ec2:DescribeSecurityGroupRules",
           "iam:GetOpenIDConnectProvider",
           "iam:GetRole",
           "iam:ListRolePolicies",
