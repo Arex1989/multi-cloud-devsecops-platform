7f53fee858493e821dfd15e748535a39e1cc0112
Arex1989 <rexmond.anih@gmail.com>
2026-09-21 17:47:48 +0200

    feat: add Azure OIDC CI and remote Terraform state

--- terraform/azure/providers.tf
+++ terraform/azure/providers.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-azure-enterprise-dev"
+    storage_account_name = "tfstate2f877566"
+    container_name       = "tfstate"
+    key                  = "azure-dev.tfstate"
+  }
+}
+
 provider "azurerm" {
   features {}
 }
