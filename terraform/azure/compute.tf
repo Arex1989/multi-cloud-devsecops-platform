eac6f43f546e1fe3fa46e9789b4a0922bea1a88d
Arex1989 <rexmond.anih@gmail.com>
2026-09-21 14:36:27 +0200

    feat: add Azure compute layer with SSH and Nginx deployment

---
+++ terraform/azure/compute.tf
@@ -0,0 +1,75 @@
+resource "azurerm_public_ip" "web" {
+  name                = "pip-multicloud-dev-web"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+
+  tags = local.common_tags
+}
+
+resource "azurerm_network_interface" "web" {
+  name                = "nic-multicloud-dev-web"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "web-ipconfig"
+    subnet_id                     = azurerm_subnet.web.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.web.id
+  }
+
+  tags = local.common_tags
+}
+
+resource "azurerm_linux_virtual_machine" "web" {
+  name                = "vm-multicloud-dev-web"
+  resource_group_name = data.azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.main.location
+  size                = "Standard_D2nls_v6"
+  admin_username      = "azureadmin"
+
+  network_interface_ids = [
+    azurerm_network_interface.web.id
+  ]
+
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = "azureadmin"
+    public_key = file(pathexpand("~/.ssh/multicloud_azure_ed25519.pub"))
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
+    version   = "latest"
+  }
+
+  custom_data = base64encode(<<-CLOUDINIT
+    #cloud-config
+    package_update: true
+    packages:
+      - nginx
+
+    runcmd:
+      - systemctl enable nginx
+      - systemctl start nginx
+      - bash -c 'echo "<html><head><title>Multi-Cloud DevSecOps Platform</title></head><body><h1>Multi-Cloud DevSecOps Platform</h1><p>Azure Web Tier - Terraform managed</p><p>Nginx deployment completed automatically.</p></body></html>" > /var/www/html/index.html'
+      - systemctl restart nginx
+  CLOUDINIT
+  )
+
+  tags = local.common_tags
+}
--- terraform/azure/security-groups.tf
+++ terraform/azure/security-groups.tf
@@ -3,19 +3,21 @@ resource "azurerm_network_security_group" "web" {
   location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
 
-  security_rule {
-    name                       = "Allow-HTTPS-Inbound"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "443"
-    source_address_prefix      = "Internet"
-    destination_address_prefix = "*"
-  }
-
   tags = local.common_tags
+}
+
+resource "azurerm_network_security_rule" "web_https" {
+  name                        = "Allow-HTTPS-Inbound"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "Internet"
+  destination_address_prefix  = "*"
+  resource_group_name         = data.azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.web.name
 }
 
 resource "azurerm_network_security_group" "application" {
@@ -59,4 +61,18 @@ resource "azurerm_subnet_network_security_group_association" "application" {
 resource "azurerm_subnet_network_security_group_association" "management" {
   subnet_id                 = azurerm_subnet.management.id
   network_security_group_id = azurerm_network_security_group.management.id
+}
+
+resource "azurerm_network_security_rule" "web_ssh_admin" {
+  name                        = "Allow-SSH-Admin"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "61.8.130.111/32"
+  destination_address_prefix  = "*"
+  resource_group_name         = data.azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.web.name
 }
